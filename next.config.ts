60746cea94cce4c92ab034e5af0f733ed0153328
LostaMasta45 <reza.nur.h45@gmail.com>
2026-02-11 19:39:17 +0700

    xzc

--- next.config.ts
+++ next.config.ts
@@ -20,6 +20,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "placehold.co",
       },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
     ],
     formats: ['image/avif', 'image/webp'],
   },
